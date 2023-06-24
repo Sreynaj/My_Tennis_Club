39433ad0f57793956d05c5ceb77c5a688bd6d586
SREYNAJ KEB <101303611+Sreynaj@users.noreply.github.com>
2023-06-24 16:11:18 +0700

    done

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v)8a_j8ltc_st+nb3&9=cecsc97n3=d+w26sp4@!_qz82*eun$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
